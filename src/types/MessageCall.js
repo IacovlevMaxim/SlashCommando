409bcfb1e0a151a5db5d82a37f615f2399771ed8
IacovlevMaxim <iacovlev.maxim@gmail.com>
2021-09-04 19:45:50 +0300

    Fixed MessageCall.commandName

--- src/types/MessageCall.js
+++ src/types/MessageCall.js
@@ -5,7 +5,7 @@ class MessageCall extends Message {
         super(client, data);
 
         this.commandType = 'message';
-        this.commandName = super.toString().split(' ').shift().split(this.client.prefix)[1].toLowerCase();
+        this.commandName = this.content.split(' ').shift().split(this.client.prefix)[1].toLowerCase();
         this.user = this.author;
         this.replied = false;
         this.deferred = false;
